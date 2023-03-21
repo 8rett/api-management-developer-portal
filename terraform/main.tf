a3fe8c7fccee11f3702b56cac907fe1d0541bf41
Brett Capewell <brettcapewell@outlook.com>
2023-03-21 12:57:38 +0000

    updated pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "terraform"
+        storage_account_name = "t3rraf0rm"
+        container_name       = "terraform"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
 provider "azurerm" {
   features {}
 }
